06a1b1988e1f535e7df217054045e2d1cb40b82a
isaacbello <isaac@teachino.io>
2024-02-28 06:00:27 +0100

    started work on the landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Quicksand", "sans-serif"],
+        serif: ["COCOGOOSE", "serif"],
+      },
+      colors: {
+        blue: {
+          100: '#4467FF',
+        },
+        pink: {
+          100: "#FF93D6"
+        },
+        yellow: {
+          100: "#fffac4",
+          200: "#fefe08",
+        }
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-landing": "linear-gradient(180deg, #fffac4 0%, #fefe08 100%)",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
